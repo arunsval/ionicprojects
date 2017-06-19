b82e09a3124382ed78cc9b0b33609c8645f9c953
Arun Selvam P M <arun666selvam@gmail.com>
2017-06-18 22:42:57 -0400

    version 1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,1 +1,22 @@
 angular.module('starter.controllers',[])
+.controller('LoginCtrl',function($scope,LoginService,$ionicPopup,$state){
+  $scope.data = {};
+  $scope.login = function(){
+    LoginService.loginUser($scope.data.username,$scope.data.password)
+    .success(function(data){
+      $scope.data.username = '',
+      $scope.data.password = '',
+      $state.go('home');
+    })
+    .error(function(data){
+      var alertPopup = $ionicPopup.alert({
+        title: 'Login failed!',
+        template: 'Please check your credentials!'
+      });
+
+    });
+
+
+    console.log("Login User:"+ $scope.data.username + "-Pw:"+ $scope.data.password);
+  }
+})
